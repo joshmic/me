01a3bfc254986e68d13927e8c9c416320256ca16
joshmic <josh.micallef05@gmail.com>
2021-07-17 09:18:14 +1000

    Set 4 Exercises

--- set4/exercise1.py
+++ set4/exercise1.py
@@ -7,6 +7,8 @@ import requests
 import inspect
 import sys
 
+from requests.api import request
+
 # Handy constants
 LOCAL = os.path.dirname(os.path.realpath(__file__))  # the context of this file
 CWD = os.getcwd()  # The curent working directory
@@ -33,10 +35,17 @@ def get_some_details():
          dictionary, you'll need integer indeces for lists, and named keys for
          dictionaries.
     """
-    json_data = open(LOCAL + "/lazyduck.json").read()
+    with open(LOCAL + "/lazyduck.json", "r", encoding="utf-8") as f:
+        json_data = f.read()
 
     data = json.loads(json_data)
-    return {"lastName": None, "password": None, "postcodePlusID": None}
+
+    last = data["results"][0]["name"]["last"]
+    password = data["results"][0]["login"]["password"]
+    postcode = data["results"][0]["location"]["postcode"]
+    id = int(data["results"][0]["id"]["value"])
+
+    return {"lastName": last, "password": password, "postcodePlusID": postcode + id}
 
 
 def wordy_pyramid():
@@ -73,17 +82,36 @@ def wordy_pyramid():
     ]
     TIP: to add an argument to a URL, use: ?argName=argVal e.g. &wordlength=
     """
-    pass
+    pyramid = []
+    for x in range(3, 20, 2):
+        url = (
+            "https://us-central1-waldenpondpress.cloudfunctions.net/"
+            "give_me_a_word?"
+            f"wordlength={x}"
+        )
+        r = requests.get(url)
+        word = r.text
+        pyramid.append(word)
+    for x in range(20, 3, -2):
+        url = (
+            "https://us-central1-waldenpondpress.cloudfunctions.net/"
+            "give_me_a_word?"
+            f"wordlength={x}"
+        )
+        r = requests.get(url)
+        word = r.text
+        pyramid.append(word)
+    return pyramid
 
 
 def pokedex(low=1, high=5):
-    """ Return the name, height and weight of the tallest pokemon in the range low to high.
+    """Return the name, height and weight of the tallest pokemon in the range low to high.
 
     Low and high are the range of pokemon ids to search between.
     Using the Pokemon API: https://pokeapi.co get some JSON using the request library
     (a working example is filled in below).
     Parse the json and extract the values needed.
-    
+
     TIP: reading json can someimes be a bit confusing. Use a tool like
          http://www.jsoneditoronline.org/ to help you see what's going on.
     TIP: these long json accessors base["thing"]["otherThing"] and so on, can
@@ -92,10 +120,28 @@ def pokedex(low=1, high=5):
     """
     template = "https://pokeapi.co/api/v2/pokemon/{id}"
 
-    url = template.format(id=5)
-    r = requests.get(url)
-    if r.status_code is 200:
-        the_json = json.loads(r.text)
+    some_pokemon = []
+    for p in range(low, high):
+        url = template.format(id=p)
+        r = requests.get(url)
+        if r.status_code == 200:
+            the_json = json.loads(r.text)
+            some_pokemon.append(the_json)
+
+    height_of_tallest_pokemon = 0
+    tallest_pokemon = "🐉"
+    for p in some_pokemon:
+        height = p["height"]
+        if height > height_of_tallest_pokemon:
+            height_of_tallest_pokemon = height
+            tallest_pokemon = p
+
+    return {
+        "name": tallest_pokemon["name"],
+        "weight": tallest_pokemon["weight"],
+        "height": tallest_pokemon["height"],
+    }
+
     return {"name": None, "weight": None, "height": None}
 
 
@@ -113,7 +159,19 @@ def diarist():
          the test will have nothing to look at.
     TIP: this might come in handy if you need to hack a 3d print file in the future.
     """
-    pass
+    mode = "r"
+    count = 0
+    with open("set4/Trispokedovetiles(laser).gcode", mode, encoding="utf-8") as gc:
+        lines = gc.readlines()
+
+    for line in lines:
+        if "M10 P1" in line:
+            count += 1
+
+    print(count)
+    laser = open("set4\lasers.pew", mode="w+")
+    laser.write(str(count))
+    laser.close()
 
 
 if __name__ == "__main__":
@@ -127,5 +185,5 @@ if __name__ == "__main__":
             print(function())
         except Exception as e:
             print(e)
-    if not os.path.isfile("lasers.pew"):
+    if not os.path.isfile("set4\lasers.pew"):
         print("diarist did not create lasers.pew")
--- set4/readme.md
+++ set4/readme.md
@@ -1,1 +1,17 @@
 TODO: Reflect on what you learned this week and what is still unclear.
+
+https://docs.python.org/3/tutorial/inputoutput.html#reading-and-writing-files
+
+Using file IO, from the docs:
+"The first argument is a string containing the filename. The second
+argument is another string containing a few characters describing the
+way in which the file will be used. mode can be 'r' when the file will
+only be read, 'w' for only writing (an existing file with the same name
+will be erased), and 'a' opens the file for appending; any data written
+to the file is automatically added to the end. 'r+' opens the file for
+both reading and writing. The mode argument is optional; 'r' will be
+assumed if it's omitted."
+
+python ..\course\set4\tests.py
+
+Very difficult to understand. Definitely need to study this topic
